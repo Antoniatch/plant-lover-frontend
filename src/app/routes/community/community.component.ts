24384489561b8c8b50a8bb5149972e6b98b04d3c
Antonia <antoniatch@zohomail.eu>
2024-01-23 16:00:17 +0100

    community page

--- src/app/routes/community/community.component.ts
+++ src/app/routes/community/community.component.ts
@@ -1,9 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, inject } from '@angular/core';
+import { IUser } from '../../types/interfaces';
+import { Subscription } from 'rxjs';
+import { Apollo } from 'apollo-angular';
+import { GET_ALL_USERS } from '../../graphql/queries';
+import { ApolloQueryResult } from '@apollo/client';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
-    selector: 'app-community',
-    standalone: true,
-    templateUrl: './community.component.html',
-    styleUrl: './community.component.sass'
+  selector: 'app-community',
+  standalone: true,
+  templateUrl: './community.component.html',
+  styleUrl: './community.component.sass',
+  imports: [CardComponent],
 })
-export class CommunityComponent {}
+export class CommunityComponent implements OnInit {
+  users!: IUser[];
+
+  getAllUsersSubscription!: Subscription;
+
+  apollo = inject(Apollo);
+
+  ngOnInit(): void {
+    this.apollo
+      .watchQuery({
+        query: GET_ALL_USERS,
+      })
+      .valueChanges.subscribe(({ data }: ApolloQueryResult<any>) => {
+        if (data) this.users = data.getAllUsers;
+      });
+  }
+}
